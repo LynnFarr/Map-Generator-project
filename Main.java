a2c11dd03e2a4fea446e2db4ccd670b5e5a06678
HunterC132 <49768593+HunterC132@users.noreply.github.com>
2019-05-11 04:11:06 -0700

    Add files via upload
    Commented

--- Main.java
+++ Main.java
@@ -1,7 +1,6 @@
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -9,17 +8,13 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
-import java.awt.*;
 
-
-public class Main extends Application implements EventHandler<ActionEvent> {
+public class Main extends Application implements EventHandler<ActionEvent>{
 
     public static final int WIDTH = 500;
     public static final int HEIGHT = 400;
@@ -43,35 +38,45 @@ public class Main extends Application implements EventHandler<ActionEvent> {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        //FIRST WINDOW
+
+        //At the start of the program
+        //The grid window doesn't show.
         generated = false;
 
+        //This connects everything together
         root = new Group();
         button = new Button();
+        //This is the text that is displayed ontop of the button
         button.setText("Enter");
+        //whenever the button is pressed it does something
         button.setOnAction(this);
 
+        //This text is initially in the text field
         imageName = new TextField("What Image?");
         imageX = new TextField("X Position");
         imageY = new TextField("Y Position");
 
+        //This is the drop box for all the diffrent sizes of scales
+        roomSize = new ListView<String>();
+        roomSize.getItems().addAll("Halfling", "Human", "Giant", "Dragon");
 
-        HBox layout = new HBox();
+        //This arranges the layout in the controller window
+        FlowPane layout = new FlowPane();
         layout.getChildren().add(imageName);
         layout.getChildren().add(imageX);
         layout.getChildren().add(imageY);
         layout.getChildren().add(button);
-
-        roomSize = new ListView<String>();
-        roomSize.getItems().addAll("Halfling", "Human", "Giant", "Dragon");
-
         layout.getChildren().add(roomSize);
-        //layout.setAlignment(Pos.TOP_LEFT);
 
+        //adds the entire layout to the root
         root.getChildren().add(layout);
+
+        //debuging purposes
         System.out.println(imageName.getCharacters());
 
-
-        Scene scene = new Scene(root, 490, 25);
+        //makes a new scene and sets it as black background
+        Scene scene = new Scene(root, 300, 200);
         scene.setFill(Color.BLACK);
         primaryStage.setTitle("Controller");
         primaryStage.setScene(scene);
@@ -79,7 +84,12 @@ public class Main extends Application implements EventHandler<ActionEvent> {
 
         ///////////////////////////////////////////////////////////
 
+        //SECOND WINDOW
+
+        //This connects everything together
         root2 = new Group();
+
+        //Creates a grid to scale
         for (int i = 0; i < WIDTH; i = i + SCALE){
             root2.getChildren().add(new Line(i, 0, i, HEIGHT));
         }
@@ -87,33 +97,39 @@ public class Main extends Application implements EventHandler<ActionEvent> {
             root2.getChildren().add(new Line(0, i, WIDTH, i));
         }
 
+        //Makes the second window with a green background (for contrast)
         Scene scene2 = new Scene(root2, WIDTH, HEIGHT);
         scene2.setFill(Color.GREEN);
         secondStage = new Stage();
         secondStage.setScene(scene2);
         secondStage.setTitle("Grid");
-        //secondStage.show();
-
     }
 
+    //This runs every time a button is clicked
     @Override
     public void handle(ActionEvent event) {
+        //enter button
         if(event.getSource() == button){
+            //prints the name of the file we typed in for debuging purposes
             System.out.println(imageName.getCharacters());
 
+            //loads up image, puts it where we want it, and shrinks it to scale
             imageView = new ImageView(imageName.getCharacters() + ".png");
-
             imageView.setX(Integer.parseInt(imageX.getCharacters().toString()) * SCALE);
             imageView.setY(Integer.parseInt(imageY.getCharacters().toString()) * SCALE);
-
             imageView.setFitHeight(SCALE);
             imageView.setFitWidth(SCALE);
+
+            //adds the image to the second window
             root2.getChildren().add(imageView);
             if(!generated){
+                //if the second window has not
+                //appeared yet, make it appear
                 generated = true;
                 secondStage.show();
             }
         }
 
     }
+
 }
