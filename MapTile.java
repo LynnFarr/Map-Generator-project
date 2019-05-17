c0bfbdc5b845a1435631a14e9122fc529154ae57
LynnFarr <46731628+LynnFarr@users.noreply.github.com>
2019-05-17 16:10:27 -0700

    Add files via upload

--- MapTile.java
+++ MapTile.java
@@ -4,23 +4,44 @@
  
 // Each MapTile represents a 2.5 ft x 2.5 ft segment of the created map
    public class MapTile {
-   public String name;           // name of png file MapTile
-// ****                                                                                 somehow need png image fxnode to link to maptile object
    public String environment;    // S = subterrenean, adding other codes as needed
-   public String buildtype;      // N = natural, C = constructed, adding others as needed
+   public String buildType;      // N = natural, C = constructed, adding others as needed
+   public int cellNum;           // cell location on grid
    public boolean northFace;     // 0(true) = open, 1(false) = closed, 
    public boolean eastFace;      // 0(true) = open, 1(false) = closed, 
    public boolean southFace;     // 0(true) = open, 1(false) = closed, 
    public boolean westFace;      // 0(true) = open, 1(false) = closed, 
-// ****                                                                      if partials need to be added these will becomea string not a boolean
     
 
 //  Constructs a Individual MapTile and populates initial state */
-    public MapTile(String name/*, fxnode for png image*/) {       //                    uncomment and modify when fxnodes for graphic available
-      this.name = name;
-//       this.tileGraphic = tileGraphic;                          //                    uncomment and modify when fxnodes for graphic available
-      northFace = (name.substring(2,3) = "0");
-      eastFace = (name.substring(3,4) = "0");
-      southFace = (name.substring(4,5) = "0");
-      westFace = (name.substring(5,6) = "0");
+    public MapTile(String environment, String buildType, int cellNum) {       
+      this.environment = environment;
+      this.buildType = buildType;
+      this.cellNum = cellNum;
+      this.northFace = true;
+      this.eastFace = true;
+      this.southFace = true;
+      this.westFace = true;
     }
+
+//  Sets north edge of block to open or closed
+    public void setNorth(boolean northFace) {       
+      this.northFace = northFace;
+    }
+
+//  Sets east edge of block to open or closed
+    public void setEast(boolean eastFace) {       
+      this.eastFace = eastFace;
+    }
+
+//  Sets south edge of block to open or closed
+    public void setSouth(boolean southFace) {       
+      this.southFace = southFace;
+    }
+
+//  Sets east edge of block to open or closed
+    public void setWest(boolean westFace) {       
+      this.westFace = westFace;
+    }
+
+}
