9bbb3d611d2cf3bc1cfa671d1e8fcf07027bf243
LynnFarr <46731628+LynnFarr@users.noreply.github.com>
2019-05-20 23:26:21 -0700

    Add files via upload

--- MapTile.java
+++ MapTile.java
@@ -1,11 +1,13 @@
 // Map Generator project 
-// Ben & Lynn 
+// Lynn 
 // tile object 
  
 // Each MapTile represents a 2.5 ft x 2.5 ft segment of the created map
    public class MapTile {
    public String environment;    // S = subterrenean, adding other codes as needed
    public String buildType;      // N = natural, C = constructed, adding others as needed
+   public int cellX;             // cell location on X axis
+   public int cellY;             // cell location on Y axis
    public int cellNum;           // cell location on grid
    public boolean northFace;     // 0(true) = open, 1(false) = closed, 
    public boolean eastFace;      // 0(true) = open, 1(false) = closed, 
@@ -14,9 +16,11 @@
     
 
 //  Constructs a Individual MapTile and populates initial state */
-    public MapTile(String environment, String buildType, int cellNum) {       
+    public MapTile(String environment, String buildType, int cellNum, int cellX, int cellY) {       
       this.environment = environment;
       this.buildType = buildType;
+      this.cellX = cellX;
+      this.cellY = cellY;
       this.cellNum = cellNum;
       this.northFace = true;
       this.eastFace = true;
@@ -42,6 +46,24 @@
 //  Sets east edge of block to open or closed
     public void setWest(boolean westFace) {       
       this.westFace = westFace;
+    }
+
+//  returns true is location is rock wall
+    public boolean isWall() {       
+      return (northFace && eastFace && southFace && westFace);
+    }
+
+//  returns name of png image best matching map position
+    public String imageName() {       
+      String image = "SN";
+
+      if (northFace) { image += "0"; } else { image += "1"; }
+      if (eastFace) { image += "0"; } else { image += "1"; }
+      if (southFace) { image += "0"; } else { image += "1"; }
+      if (westFace) { image += "0"; } else { image += "1"; }
+      
+      image += "a.png";
+      return (image);
     }
 
 }
