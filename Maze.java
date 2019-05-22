57362a6b8e6753a801e770ab1864b59687592e59
Ben <49768587+benclay57@users.noreply.github.com>
2019-05-22 13:00:02 -0700

    Maze Draft 2
    I changed the constructor, and I modified the disjoint so it can move left/right/down. Still need exceptions for when it goes till the end of each side of the row.

--- Maze.java
+++ Maze.java
@@ -10,36 +10,84 @@ import java.util.*; //imports for random num
 
 public class Maze {
    private int numTile; 
-   private int tiles[] = new int[numTile];
+   private int index[] = new int[8000];
+   private MapTile objects[] = new MapTile[8000];
 
    //*Constructs Maze data strcuture*//
     public Maze(int numTile) {    
          this.numTile = numTile;
-         
+         for(int i=0; i<8000; i++){
+            MapTile element = new MapTile("S","N",i);
+            objects[i] = element; //fills array w/ 7999 base objects
+            index[i] = i; //establishs sepperate sets
+         }
                                
     }//end of Maze Constructor
     
-    //8creates the maze data strcuture itself*
+    //*creates the maze data strcuture itself(builds top-down maze)*
     public void create(){
-      Random rand = new Random();
-      int randomNum = rand.nextInt(2) +1; //creates random number to compare with
-      int tileRight = 0; 
-      int tileBelow = 0;
-      for (int i=0; i<=tiles.length-1; i++){
+      int i = 0; //starts indexing variable
+      for(int row=0; row<800; row++){
+                  
          
-         //grabs right tile & unions it to rep element
-         if(randomNum == 1){
-            tileRight = i +1; //grabs tile to right of chosen tile
-            tiles[tileRight] = i;
-         }//
-         //grabs bottom tile & unions it to rep element
-         if(randomNum == 2){
-            tileBelow = i +5; //grabs tile to below of chosen tile
-            tiles[tileBelow] = i;
-         }//
+         boolean nextRow = false;
+         while (nextRow==false){ //keeps it on the row
+         
+         //**CHECKS**
+         
+         //Checks if on last row, creates chances
+         int current = i; //grabs current index
+         if(row==799){ //last row case
+            Random rand = new Random();
+            int randomNum = rand.nextInt(2) +1; //creates random number to compare with (excludes bottom case)
+         }
+         else{
+            Random rand = new Random();
+            int randomNum = rand.nextInt(3) +1; //creates random number to compare with
+         }
+         //
+         
+         //checks if left, right, bot already in disjoint
+         boolean left = false; //not in disjoint
+         boolean right = false;
+         boolean bot = false;
+         //
+         if(index[current -1] == index[current]){ //checks left tile
+            left = true;
+         }
+         if(index[current +1] == index[current]){ //checks right tile
+            right = true;
+         }
+         if(index[current +100] == index[current]){ //checks bot tile (oppisite if bot-up Maze)
+            bot = true;
+         }
+         //
+                  
+           //grabs right tile & unions it to rep element
+          if(randomNum == 1 && right == false){
+              i++; //moves right
+              index[i] = index[current]; //grabs right tile, places gives it the same rep value as the last tile
+              //assigns right element rep element value
+          }//
+          //grabs bottom tile & unions it to rep element
+          if(randomNum == 2 && left == false){//moves left
+               i--;
+               index[i] = index[current];
+               //assigns left element rep element value
+          }//
+          
+           if(randomNum == 3 && bot == false){ //moves down
+                i +=100; //grabs tile to below of chosen tile
+                index[i] = index[current];
+               //assigns bottom element rep element value
+               
+               nextRow = true; //switches row
+          }//
 
-      }//
-      System.out.println(Arrays.toString(tiles));
+          
+         }//end while loop
+       }//end of row iterator
+         System.out.println(Arrays.toString(index1));
     }//end of create method
      
 }
