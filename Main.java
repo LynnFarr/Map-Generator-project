1d38940fdf584497eabf6c599471e61c519963fe
HunterC132 <49768593+HunterC132@users.noreply.github.com>
2019-06-05 20:26:44 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,3 +1,8 @@
+//Hunter Cambridge
+//CS 240
+//Creation Date: 5/13/2019
+//Last Edited: 6/5/2019
+//Creates a randomly generated map
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -5,7 +10,6 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.paint.Color;
@@ -14,14 +18,11 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application implements EventHandler<ActionEvent>{
+    public static final int SCALE = 30;
+    public static final int WIDTH = SCALE * 40;
+    public static final int HEIGHT = SCALE * 20;
 
-    public static final int WIDTH = 500;
-    public static final int HEIGHT = 400;
-    public static final int SCALE = 50;
 
-    private static TextField imageName;
-    private static TextField imageX;
-    private static TextField imageY;
     private static Stage secondStage;
     private static Button button;
     private static ImageView imageView;
@@ -46,32 +47,16 @@ public class Main extends Application implements EventHandler<ActionEvent>{
         root = new Group();
         button = new Button();
         //This is the text that is displayed ontop of the button
-        button.setText("Enter");
+        button.setText("Generate new Dungeon");
         //whenever the button is pressed it does something
         button.setOnAction(this);
 
-        //This text is initially in the text field
-        imageName = new TextField("What Image?");
-        imageX = new TextField("X Position");
-        imageY = new TextField("Y Position");
-
-        //This is the drop box for all the diffrent sizes of scales
-        roomSize = new ListView<String>();
-        roomSize.getItems().addAll("Halfling", "Human", "Giant", "Dragon");
-
         //This arranges the layout in the controller window
         FlowPane layout = new FlowPane();
-        layout.getChildren().add(imageName);
-        layout.getChildren().add(imageX);
-        layout.getChildren().add(imageY);
         layout.getChildren().add(button);
-        layout.getChildren().add(roomSize);
 
         //adds the entire layout to the root
         root.getChildren().add(layout);
-
-        //debuging purposes
-        System.out.println(imageName.getCharacters());
 
         //makes a new scene and sets it as black background
         Scene scene = new Scene(root, 300, 200);
@@ -108,18 +93,26 @@ public class Main extends Application implements EventHandler<ActionEvent>{
     public void handle(ActionEvent event) {
         //enter button
         if(event.getSource() == button){
-            //prints the name of the file we typed in for debuging purposes
-            System.out.println(imageName.getCharacters());
 
-            //loads up image, puts it where we want it, and shrinks it to scale
-            imageView = new ImageView(imageName.getCharacters() + ".png");
-            imageView.setX(Integer.parseInt(imageX.getCharacters().toString()) * SCALE);
-            imageView.setY(Integer.parseInt(imageY.getCharacters().toString()) * SCALE);
-            imageView.setFitHeight(SCALE);
-            imageView.setFitWidth(SCALE);
+            Integrate mazeMaker = new Integrate();
+            String[] maze = mazeMaker.fill();
+            System.out.println(maze.length);
 
-            //adds the image to the second window
-            root2.getChildren().add(imageView);
+            for(int i = 0; i < maze.length; i ++){
+                String bleh = maze[i];
+
+                imageView = new ImageView(bleh + "a.png");
+                imageView.setX(i % 40 * SCALE);
+                int y = i / 40;
+                System.out.println(i);
+                System.out.println(y);
+                imageView.setY(y * SCALE);
+                imageView.setFitHeight(SCALE);
+                imageView.setFitWidth(SCALE);
+                root2.getChildren().add(imageView);
+            }
+
+
             if(!generated){
                 //if the second window has not
                 //appeared yet, make it appear
